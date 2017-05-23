01dea13a5586f7c46743f20301f1e7315941daed
Michael Marolt <michael.marolt@edu.campus02.at>
2017-05-23 20:18:20 +0200

    Startseite: Layout und fixe Texte

--- app/src/main/java/campus02/iwi2014/timeeey/MainActivity.java
+++ app/src/main/java/campus02/iwi2014/timeeey/MainActivity.java
@@ -1,5 +1,7 @@
 package campus02.iwi2014.timeeey;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,17 +10,74 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtName;
+    TextView txtCompany;
+    TextView txtDay;
+    TextView txtDate;
+    TextView txtDescription;
+
+    String test = "";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
+        // Fixe Werte
+        txtDate = (TextView) findViewById(R.id.txtDate);
+        Timer t = new Timer();
+        /*
+        Funktioniert noch nicht
+
+        t.schedule(new TimerTask() {
+            @Override public void run() {
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+                Date currentDate = new Date();
+                txtDate.setText(dateFormat.format(currentDate));
+            }
+        }, 0L, 1000L);*/
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        Date currentDate = new Date();
+        txtDate.setText(dateFormat.format(currentDate));
+
+
+
+
+        txtDay = (TextView)findViewById(R.id.txtDay);
+        txtDay.setText(Calendar.getInstance().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.GERMAN));
+
+
+        // Dummy-Werte
+        txtName = (TextView)findViewById(R.id.txtName);
+        txtName.setText("Max Mustermann");
+
+        txtCompany = (TextView)findViewById(R.id.txtCompany);
+        txtCompany.setText("Mustermann GmbH");
+
+        txtDescription = (TextView)findViewById(R.id.txtDescription);
+        txtDescription.setText("Mustermann GmbH\nTest\nabc");
+
+        txtDay.setTextColor(Color.BLACK);
+        txtDate.setTextColor(Color.BLACK);
+        txtName.setTextColor(Color.BLACK);
+        txtCompany.setTextColor(Color.BLACK);
+        txtDescription.setTextColor(Color.BLACK);
     }
 
     @Override
